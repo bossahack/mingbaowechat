fd4d7ac65f7e5c4747ebee07a1af9a915d72eabe
holly fred <bossahack@gmail.com>
2019-09-28 16:36:43 +0800

    new

---
+++ pages/business/product/product.js
@@ -0,0 +1,9 @@
+
+Component({
+  data: {
+    types: [{ Id: 1, Name: '招牌' }, { Id: 1, Name: '招牌' }, { Id: 1, Name: '招牌' }, { Id: 1, Name: '招牌' }, { Id: 1, Name: '招牌' }, { Id: 1, Name: '招牌' }, { Id: 1, Name: '招牌' }]
+  },
+  methods: {
+
+  }
+})
