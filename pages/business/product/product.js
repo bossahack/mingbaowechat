49c68abfa749cc2196e2f3e930e19465c4da4ef2
holly fred <bossahack@gmail.com>
2019-10-20 16:16:48 +0800

    移除测试文件

--- pages/business/product/product.js
+++
@@ -1,9 +0,0 @@
-
-Component({
-  data: {
-    types: [{ Id: 1, Name: '招牌' }, { Id: 1, Name: '招牌' }, { Id: 1, Name: '招牌' }, { Id: 1, Name: '招牌' }, { Id: 1, Name: '招牌' }, { Id: 1, Name: '招牌' }, { Id: 1, Name: '招牌' }]
-  },
-  methods: {
-
-  }
-})
