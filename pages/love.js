40497493681beac6100045c653fbd00a3dc6b44e
licun <licun@benlai.com>
2020-04-16 10:59:29 +0800

    推荐人

--- pages/love.js
+++ pages/love.js
@@ -25,31 +25,38 @@ Page({
     }, 300);
     
 
-    //测试完成后删除此代码
+    //扫码进入
     let that=this;
     if (options != null & options.scene != null) {
       clearInterval(timeInterval);
-      if (options.scene.startsWith("id")) {
-        var timeInterval = setInterval(function () {
+      if (options.scene.startsWith("id")) {//扫店铺码
+        var timeInterval1 = setInterval(function () {
           var token = wx.getStorageSync("token");
           if (!token)
             return;
+          clearInterval(timeInterval1);
           var scene = decodeURIComponent(options.scene);
           var id = scene.replace("id", "");
           that.collectShop(id);
-          clearInterval(timeInterval);
+          that.recommendByShop(id);
+          wx.switchTab({
+            url: 'home/home',
+          });
         }, 200);
 
       }
-      if (options.scene.startsWith("user")) {
-        var timeInterval = setInterval(function () {
+      if (options.scene.startsWith("user")) {//扫用户码进入
+        var timeInterval2 = setInterval(function () {
           var token = wx.getStorageSync("token");
           if (!token)
             return;
+          clearInterval(timeInterval2);
           var scene = decodeURIComponent(options.scene);
           var id = scene.replace("user", "");
           that.recommend(id);
-          clearInterval(timeInterval);
+          wx.switchTab({
+            url: 'home/home',
+          });
         }, 200);
       }
     }
@@ -61,6 +68,10 @@ Page({
   },
   recommend(userId) {
     app.httpPost("user/Recommend?userId=" + userId, null, function (result) {
+    });
+  },
+  recommendByShop(shopId) {
+    app.httpPost("user/RecommendByShop?shopId=" + shopId, null, function (result) {
     });
   },
   /**
