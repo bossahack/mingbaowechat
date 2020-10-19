93aeead98a415fca28f2afbdf338ad5cb771a1fc
licun <licun@benlai.com>
2020-10-19 19:47:08 +0800

    更新 电话

--- pages/shop/shop.js
+++ pages/shop/shop.js
@@ -317,7 +317,7 @@ methods:{
       });
       return;
     }
-    if (!RegExp("^[1][3,4,5,7,8][0-9]{9}\$").test(phone)){
+    if (!RegExp("^[1][2,3,4,5,6,7,8,9][0-9]{9}\$").test(phone)){
       wx.showToast({
         title: '手机号格式不正确，请检查',
         icon: 'none'
