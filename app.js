2bfdd28ff496f7e870e9b78f7734c75d94b836a7
holly fred <1241205407@11.com>
2020-10-19 23:06:43 +0800

    更新

--- app.js
+++ app.js
@@ -8,17 +8,13 @@ App({
     if (wx.canIUse('getUpdateManager')) {
       const updateManager = wx.getUpdateManager()
       updateManager.onCheckForUpdate(function (res) {
-        // 请求完新版本信息的回调
         if (res.hasUpdate) {
           updateManager.onUpdateReady(function () {
             wx.showModal({
               title: '更新提示',
               content: '新版本已经准备好，是否重启应用？',
               success: function (res) {
-                console.log('success====', res)
-                if (res.confirm) {
                   updateManager.applyUpdate()
-                }
               }
             })
           })
